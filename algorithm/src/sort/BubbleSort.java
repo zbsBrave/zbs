18fbb8a81981c47e3165b157be5ca4288a7066e2
zbs <2829024592@qq.com>
2020-11-23 13:26:05 +0800

    选择排序

--- algorithm/src/sort/BubbleSort.java
+++ algorithm/src/sort/BubbleSort.java
@@ -2,18 +2,36 @@ package sort;
 
 import java.util.Arrays;
 
+/**
+ * 冒泡排序
+ * 两两交换
+ */
 public class BubbleSort {
-    public static void bubbleSort(int[] arr){
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]) Util.swap(arr,j,j+1);
-            }
-        }
-    }
-
     public static void main(String[] args) {
         int[] arr = {6,1,3,2,10,13,5,7};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
+    }
+    //将 最大/小值 移动到最右边
+    public static void bubbleSort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){//遍历的次数
+            for(int j=0; j<arr.length-i-1; j++){//依次比较两个数的大小
+                if(arr[j]<arr[j+1]) Util.swap(arr,j,j+1);
+            }
+        }
+    }
+
+    //优化：当遍历一遍都没有发生交换时，其实已经可以认为数组已经排序好了
+    public static void bubbleSort1(int[] arr) {
+        for(int i=0; i<arr.length-1; i++) {//遍历的次数
+            boolean flag = true;
+            for(int j=0; j<arr.length-1-i; j++) {//依次比较两个数的大小
+                if(arr[j] < arr[j+1]) {
+                    Util.swap(arr,j,j=1);
+                    flag = false;//发生交换
+                }
+            }
+            if(flag) return;//如果未发生交换，代表已经排序完，直接返回
+        }
     }
 }
---
+++ algorithm/src/sort/SelectionSort.java
@@ -0,0 +1,27 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ * 找出数组最大的元素，和最后一位交换位置
+ * 不稳定算法
+ * @author zbs
+ * @since  2020/11/23
+ */
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {1,5,3,9,2,4,7};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void selectionSort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int max=i,temp=0;// max记录当前趟最大值的角标,初始值是最左边
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j] > arr[max]) max=j;
+            }
+            Util.swap(arr,i,max);
+        }
+    }
+}
