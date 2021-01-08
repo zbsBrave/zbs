691c6e705bb2e5393bdac76e965e777f18bf217c
zhangbaisen <zhangbaisen@xy51.com>
2021-01-08 11:03:18 +0800

    generator项目初始化

---
+++ generator-zbs/src/main/java/zbs/GeneratorMain.java
@@ -0,0 +1,16 @@
+package zbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorMain {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GeneratorMain.class, args);
+
+
+    }
+
+}
