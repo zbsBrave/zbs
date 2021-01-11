f4430227bfbc17f7e8eb82b3080140b65a193926
zbs <2829024592@qq.com>
2021-01-11 21:27:53 +0800

    websocket 初始化

---
+++ websocket-zbs/src/main/java/zbs/controller/WebsocketController.java
@@ -0,0 +1,11 @@
+package zbs.controller;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zbs
+ * @since 2021/1/10
+ */
+@RestController
+public class WebsocketController {
+
+}
