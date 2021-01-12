84040a8d278dbb1fd517c7579a6eb3e9daefe5d8
zhangbaisen <zhangbaisen@xy51.com>
2021-01-12 09:59:05 +0800

    合并master

---
+++ websocket-zbs/src/main/java/zbs/WebsocketMain.java
@@ -0,0 +1,13 @@
+package zbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketMain.class, args);
+    }
+
+}
